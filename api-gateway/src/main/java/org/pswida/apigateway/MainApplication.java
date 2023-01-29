e2adc74956ba4167ef6d4d6616b26dd3186cfc8b
Przemek Świda <przemyslaw.swida@boldare.com>
2023-01-29 21:38:12 +0100

    Initial commit

---
+++ api-gateway/src/main/java/org/pswida/apigateway/MainApplication.java
@@ -0,0 +1,22 @@
+package org.pswida.apigateway;
+
+import lombok.extern.log4j.Log4j2;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@Log4j2
+@SpringBootApplication
+public class MainApplication implements CommandLineRunner {
+
+  public static void main(String[] args) {
+    SpringApplication.run(MainApplication.class, args);
+  }
+
+  @Override
+  public void run(String... args) {
+    log.info("================================================");
+    log.info("MICROSERVICES-POC: API GATEWAY");
+    log.info("================================================");
+  }
+}
